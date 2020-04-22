db72bf35470513ce44dceb4767f4a4b481ed5024
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-04-22 10:33:15 +0530

    Added:-load state csv file and handle customerised exception

--- stateCensusTest/UnitTest1.cs
+++ stateCensusTest/UnitTest1.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
+using sate_Censes;
+using stateCensusAnaliser;
 
-namespace stateCensusTest
+namespace stateCencesTesting
 {
     public class Tests
     {
@@ -8,11 +10,135 @@ namespace stateCensusTest
         public void Setup()
         {
         }
-
+        /// <summary>
+        /// check for number of record 
+        /// </summary>
         [Test]
-        public void Test1()
+        public void TestForNumberOfRecord()
         {
-            Assert.Pass();
+            //create a object of stateCensusAnalyser class 
+            stateCensusAnalyser read = new stateCensusAnalyser();
+            //give the  path of csv file
+            string path = @"D:\trimbak\state analys\StateCensusData.csv";
+            //call the readmethod and it return the number of record
+            int record = read.ReadMethod(path, ',');
+            Assert.AreEqual(29, record);
+        }
+        /// <summary>
+        /// check for when file not found 
+        /// </summary>
+        [Test]
+        public void TestForFileNotFoundException()
+        {
+            try
+            {
+                //create a object of stateCensusAnalyser class 
+                stateCensusAnalyser read = new stateCensusAnalyser();
+                //give the  path of csv file
+                string path = @"D:\trimbak\state analys\StateCensusData.csv";
+                //call the readmethod and it return the number of record
+                int record = read.ReadMethod(path, ',');
+            }
+            catch (stateCensusException e)
+            {
+                Assert.AreEqual("file is not present on this location", e.Message);
+            }
+        }
+        /// <summary>
+        /// check for wrong type of  file
+        /// </summary>
+        [Test]
+        public void TestForWrongTypeException()
+        {
+            try
+            {
+                //create a object of stateCensusAnalyser class 
+                stateCensusAnalyser read = new stateCensusAnalyser();
+                //chenge file type csv to txt and check
+                string path = @"D:\trimbak\state analys\StateCensusData.txt";
+                //call the readmethod and it return the number of record
+                int record = read.ReadMethod(path, ',');
+            }
+            catch (stateCensusException e)
+            {
+                Assert.AreEqual("enter proper file", e.Message);
+            }
+        }
+        [Test]
+        public void TestForWrongDelimeterException()
+        {
+            try
+            {
+                //create a object of stateCensusAnalyser class 
+                stateCensusAnalyser read = new stateCensusAnalyser();
+                //chenge file type csv to txt and check
+                string path = @"D:\trimbak\state analys\StateCensusData.csv";
+                //call the readmethod and it return the number of record
+                int record = read.ReadMethod(path, ';');
+            }
+            catch (stateCensusException e)
+            {
+                Assert.AreEqual("wrong delimeter, please enter proper delimeter", e.Message);
+            }
+        }
+        [Test]
+        public void TestForWrongHeaderNamesException()
+        {
+            try
+            {
+                //create a object of stateCensusAnalyser class 
+                stateCensusAnalyser read = new stateCensusAnalyser();
+                //chenge file type csv to txt and check
+                string path = @"D:\trimbak\state analys\StateCensusData.csv";
+                //call the numberofheder and it return the heder
+                //expected output
+                string[] expectedHeader = { "State", "Population", "AreaInSqKm", "DensityPerSqKm" };
+                //check for header name not proper
+                string[] userHeader = { "State", "Population", "AreaInSqKm", "DensitySqKm" };
+                string[] header = read.numberOfHeader(path, userHeader);
+                //if header name is proper then lpass the test if header name not proper then throws exception header name is not proper and catch in catcj
+                for (int i = 0; i < header.Length; i++)
+                {
+                    Assert.AreEqual(expectedHeader[i], header[i]);
+                }
+            }
+            catch (stateCensusException e)
+            {
+                Assert.AreEqual("header name is not same", e.Message);
+            }
+        }
+        /// <summary>
+        /// check for wrong header length exception
+        /// </summary>
+        [Test]
+        public void CheckForWrongHeaderLengthException()
+        {
+            try
+            {
+                //create a object of stateCensusAnalyser class 
+                stateCensusAnalyser read = new stateCensusAnalyser();
+                //chenge file type csv to txt and check
+                string path = @"D:\trimbak\state analys\StateCensusData.csv";
+                //call the readmethod and it return the number of record
+                //expected array
+                string[] expectedHeader = { "State", "Population", "AreaInSqKm", "DensityPerSqKm" };
+                //pass wrong length array
+                string[] userHeader = { "State", "Population", "AreaInSqKm" };
+                //throws the length not proper exception
+                //if length is proper and name is proper then return header names
+                //and check with expected output
+                string[] header = read.numberOfHeader(path, userHeader);
+                for (int i = 0; i < header.Length; i++)
+                {
+                    Assert.AreEqual(expectedHeader[i], header[i]);
+                }
+            }
+            catch (stateCensusException e)
+            {
+                //if string and massage same then pass the test
+                Assert.AreEqual("header length is not same", e.Message);
+            }
+
         }
     }
 }
--- stateScensus/stateCensusException.cs
+++ stateScensus/stateCensusException.cs
@@ -2,9 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace stateScensus
+namespace stateCensusAnaliser
 {
-    class stateCensusException
-    {
+        /// <summary>
+        /// create customarised enum class for handle customarised exception
+        /// </summary>
+        public class stateCensusException : Exception
+        {
+            public enum ExceptionType
+            {
+                FILE_NOT_FOUND,
+                WRONG_FILE,
+                WRONG_DELIMETER,
+                HEADER_IS_NOT_PROPER,
+                HEADER_NAME_NOT_SAME,
+                HEADER_LENGTH_NOT_SAME
+            }
+            ExceptionType type;
+            public stateCensusException(ExceptionType type, string message) : base(message)
+            {
+                this.type = type;
+            }
+
+        }
     }
-}
